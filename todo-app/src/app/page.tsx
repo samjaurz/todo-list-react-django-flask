ad5be65f5ef374aa6c22f5248c2e461f68e3226e
Samuel Jauregui <samjaurz_@hotmail.com>
2025-07-17 22:35:21 -0700

    patch

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -2,8 +2,8 @@
 import api from "@/lib/axios"
 import SearchBar from "@/components/SearhBar";
 import Table from "@/components/Table"
-import { useEffect, useState } from "react";
-import { getAllTask } from "@/services/taskService";
+import {useEffect, useState} from "react";
+import {getAllTask} from "@/services/taskService";
 
 interface Task {
     id: number;
@@ -12,78 +12,92 @@ interface Task {
 }
 
 export default function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [editingId, setEditingId] = useState<number | null>(null);
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [editingId, setEditingId] = useState<number | null>(null);
 
-  useEffect(() => {
-    getAllTask().then((data) => {
-      setTasks(data ?? []);
-    });
-  }, []);
+    useEffect(() => {
+        getAllTask().then((data) => {
+            setTasks(data ?? []);
+        });
+    }, []);
 
 
-  const handleEdit = (task: Task) => setEditingId(task.id)
+    const handleEdit = (task: Task) => setEditingId(task.id)
 
-  const handlerAdd = (addNewRow: any) => {
-    console.log("handleUpdated",addNewRow)
-    setTasks(addNewRow)
-  } 
-
-  const handleSave = async (updated: Task) => {
-    if (updated){
-        const payload = {
-        name: updated.name,
-        status: updated.status
-      }
-      if (editingId === 0) {
-        const response = await api.post('task', payload);
-        console.log("post response",response);
-        setTasks((prev) => {
-            prev = [...prev];
-            prev.shift();
-            return [...prev, response.data];
-            });
-
-      } else {
-        const response = await api.put(`task/${updated.id}`, payload);
-        console.log("update response",response);
-        setTasks((prev) => 
-        prev.map((t) => (t.id === updated.id ? updated : t))
-      );
-      }
-      setEditingId(null);
+    const handlerAdd = (addNewRow: any) => {
+        setTasks(addNewRow)
     }
-  }
+    const handleSearch = async (searchTask: string) => {
+        console.log("search handle", searchTask)
+        const response = await api.get(`task/search?name=${searchTask}`)
+        console.log("response handle search after api", response)
+        setTasks(response.data)
+    }
 
-  const handleDelete = async ( task: Task ) =>{
+    //     const filtered = tasks.filter((task) => {
+    //         return task.name.toLowerCase().includes(addNewRow.toLowerCase());
+    //     })
+    //     setTasks(filtered)
+    // }
+
+    const handleSave = async (updated: Task) => {
+        console.log("updated", typeof (updated.status))
+        if (updated) {
+            const payload = {
+                name: updated.name,
+                status: updated.status
+            }
+            if (editingId === 0) {
+                const response = await api.post('task', payload);
+                console.log("post response", response);
+                setTasks((prev) => {
+                    prev = [...prev];
+                    response.data.status = payload.status
+                    prev.shift();
+                    return [...prev, response.data];
+                });
+            } else {
+                const response = await api.put(`task/${updated.id}`, payload);
+                console.log("update response", response);
+                setTasks((prev) =>
+                    prev.map((t) => (t.id === updated.id ? updated : t))
+                );
+            }
+            setEditingId(null);
+        }
+    }
+    const handleDelete = async (task: Task) => {
         console.log("Deleting task", task.id)
         const response = await api.delete(`/task/${task.id}`);
+        console.log("delete",response)
         const filteredTasks = tasks.filter(el => el.id != task.id);
         setTasks(filteredTasks)
     }
 
-  return (
-    <div className="pl-10 pr-10">
-      <div className="justify-center items-center flex text-5xl py-10 font-bold">
-        <h1>TODO LIST</h1>
-      </div>
-      <div>
-        <SearchBar
-          tasks={tasks} 
-          onEdit={handleEdit}
-          handlerAdd={handlerAdd}
-        />
-        <main>
-        <Table
-            tasks={tasks}
-            editingId={editingId}
-            onEdit={handleEdit}
-            onSave={handleSave}
-            setEditingId={setEditingId}
-            onDelete={handleDelete}
-        />
-        </main>
-      </div>
-    </div>
-  );
+    return (
+        <div className="pl-10 pr-10">
+            <div className="justify-center items-center flex text-5xl py-10 font-bold">
+                <h1>TODO LIST</h1>
+            </div>
+            <div>
+                <SearchBar
+                    tasks={tasks}
+                    onEdit={handleEdit}
+                    handleSearch={handleSearch}
+                    handlerAdd = {handlerAdd}
+                />
+                <main>
+                    <Table
+                        tasks={tasks}
+                        editingId={editingId}
+                        onEdit={handleEdit}
+                        onSave={handleSave}
+                        setEditingId={setEditingId}
+                        onDelete={handleDelete}
+                        handlerAdd={handlerAdd}
+                    />
+                </main>
+            </div>
+        </div>
+    );
 }
--- todo-app/src/components/SearhBar.tsx
+++ todo-app/src/components/SearhBar.tsx
@@ -1,7 +1,7 @@
 "use client"
-import React from "react";
-import { PlusIcon, ArrowPathIcon } from "@heroicons/react/24/solid";
-import { searchTask } from "@/services/taskService";
+import React, {useState} from "react";
+import {PlusIcon} from "@heroicons/react/24/solid";
+
 
 interface Task {
     id: number;
@@ -12,44 +12,50 @@ interface Task {
 interface Props {
     tasks: Task[];
     handlerAdd: CallableFunction;
+    handleSearch: CallableFunction;
     onEdit: CallableFunction;
 }
 
-const SearchBar = ({ tasks, handlerAdd, onEdit}: Props) => {
-  const addTask = () => {
-    console.log("add row")
-    const new_tasks = [{
-      id: 0,
-      status: false,
-      name: ""
-    }, ...tasks];
+const SearchBar = ({tasks, handlerAdd,handleSearch, onEdit}: Props) => {
+    const [searchTask, setSearchTask] = useState<string>("")
 
-    console.log("new_task added", new_tasks)
-    handlerAdd(new_tasks)
-    onEdit(new_tasks[0])
-  }
+    const addTask = () => {
+        console.log("add row")
+        const new_tasks = [{
+            id: 0,
+            status: false,
+            name: ""
+        }, ...tasks];
 
-   
-  return (
-    <div className="w-full border border-gray-300 rounded-md p-2 bg-white shadow-sm flex gap-4 items-center ">
-      <input
-        type="text"
-        className="flex-1 border border-gray-300 rounded px-2 py-1 text-sm p-0.5"
-        placeholder="Search Task"
-        onChange={(e) => {searchTask( e.target.value);
-        }}
-      />
-      
-      <div className="flex space-x-2 items-start ">
-        <button className="bg-green-500 hover:bg-green-600 text-white p-2 rounded">
-          <PlusIcon className="h-4 w-4" onClick={addTask} />
-        </button>
-        <button className="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded">
-          <ArrowPathIcon className="h-4 w-4" />
-        </button>
-      </div>
-    </div>
-  );
+        console.log("new_task added", new_tasks)
+        handlerAdd(new_tasks)
+        onEdit(new_tasks[0])
+    }
+
+
+    const onChange = (e) => {
+        setSearchTask(e.target.value);
+        console.log("task", tasks)
+        handleSearch(searchTask)
+    }
+
+    return (
+        <div className="w-full border border-gray-300 rounded-md p-2 bg-white shadow-sm flex gap-4 items-center ">
+            <input
+                type="text"
+                className="flex-1 border border-gray-300 rounded px-2 py-1 text-sm p-0.5"
+                placeholder="Search Task"
+                value={searchTask}
+                onChange={onChange}
+            />
+
+            <div className="flex space-x-2 items-start ">
+                <button className="bg-green-500 hover:bg-green-600 text-white p-2 rounded">
+                    <PlusIcon className="h-4 w-4" onClick={addTask}/>
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default SearchBar;
