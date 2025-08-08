8b4bf12d4d57b5bfcad0a42c6025f215b00737b6
Samuel Jauregui <samjaurz_@hotmail.com>
2025-08-08 01:52:18 -0700

    feat: add hashing password to login/signup using the database

--- flask-api/backend/flask/auth/auth_controller.py
+++ flask-api/backend/flask/auth/auth_controller.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, current_app
 from backend.db_session import SessionLocal
 import bcrypt
 from backend.repositories.user_repository import UserRepository
@@ -13,18 +13,33 @@ def login():
     password_client = data['password'].encode('utf-8')
 
     user = UserRepository(session).get_user_by_email(data['email'])
-    salt, pass_in_db = user.password.split("$_$")
+    if not user:
+        return jsonify({"error": "User not found"}), 404
 
-    salt_b = bytes(salt)
-    hashed = bcrypt.hashpw(password_client, salt_b)
+    stored_hash = user.password.encode('utf-8')
 
-
-    if bcrypt.checkpw(pass_in_db, hashed):
-         return jsonify("Password is valid! User authenticated.")
-    return jsonify({
-        "from_db": pass_in_db.__str__(),
-        "from_hashed": hashed.__str__(),
-    })
+    if bcrypt.checkpw(password_client, stored_hash):
+        return jsonify({"message": "Password is valid! User authenticated."}), 20
+    return jsonify({"error": "Invalid password"}), 401
+    # data = request.get_json()
+    # password_client = data['password'].encode('utf-8')
+    #
+    # user = UserRepository(session).get_user_by_email(data['email'])
+    # salt, pass_in_db = user.password.split("$_$")
+    #
+    # salt_b = salt.encode('utf-8')
+    # pass_in_db_b = pass_in_db.encode('utf-8')
+    # hashed = bcrypt.hashpw(password_client, salt_b)
+    #
+    # if bcrypt.checkpw(hashed, pass_in_db_b):
+    #     return jsonify({"password": "pass"})
+    # if hashed == pass_in_db_b:
+    #      return jsonify("Password is valid! User authenticated."), 200
+    #
+    # return jsonify({
+    #     "from_db": pass_in_db_b.__str__(),
+    #     "from_hashed": hashed.__str__(),
+    # }), 401
 
 
 @auth_api.route('/sign_up', methods=['POST'])
@@ -43,7 +58,8 @@ def sign_up():
         name=data['name'],
         last_name=data['last_name'],
         email=data['email'],
-        password=f"{salt}$_${hashed}",
+        password=hashed.decode('utf-8'),
+        # password=f"{salt.decode('utf-8')}$_${hashed.decode('utf-8')}",
         status=data['status'],
     )
     return jsonify(created_user.to_dict())
