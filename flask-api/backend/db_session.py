1ffbcc6438da7f89249ae8665ab2936488cdc4a4
Samuel Jauregui <samjaurz_@hotmail.com>
2025-08-09 14:23:37 -0700

    test(db_session): create a decorador session to inject as dependecies

--- flask-api/backend/db_session.py
+++ flask-api/backend/db_session.py
@@ -1,11 +1,33 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from backend.model import Base as MainBase
+from functools import wraps
 
 DATABASE_URL = "postgresql://root:root@localhost:5433/todo_list"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = MainBase
+Base.metadata.create_all(bind=engine)
 
+
+def get_db():
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
+
+
+def with_db_session(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        session = None
+        try:
+            session = next(get_db())
+            return func(session, *args, **kwargs)
+        finally:
+            if session:
+                session.close()
+
+    return wrapper
