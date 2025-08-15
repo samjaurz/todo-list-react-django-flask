641e59e657e8643de6d94458912c4a49f2876ada
Samuel Jauregui <samjaurz_@hotmail.com>
2025-08-15 05:42:19 -0700

    fix(decorator): added @wraps(func) for mantein the name of the funcion

--- flask-api/backend/flask/auth/decorator.py
+++ flask-api/backend/flask/auth/decorator.py
@@ -1,7 +1,9 @@
 from backend.flask.auth.auth_jwt import JWTAuth
 from flask import request ,jsonify
+from functools import wraps
 
 def auth_decorator(func):
+    @wraps(func)
     def wrapper(session, **kwargs):
         token = request.cookies.get('access_token')
         if not token:
