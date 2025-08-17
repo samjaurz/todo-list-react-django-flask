d886fbae3f1690bbf32d957ba701e5a285e6928c
Samuel Jauregui <samjaurz_@hotmail.com>
2025-08-17 06:32:39 -0700

    feat(auth_controlle): create refresh token logic

--- flask-api/backend/flask/auth/auth_jwt.py
+++ flask-api/backend/flask/auth/auth_jwt.py
@@ -5,15 +5,21 @@ import jwt
 
 class JWTAuth:
     def __init__(self):
-        self.secret = os.getenv("SECRET", "ss")
+        self.secret = os.getenv("SECRET", "SECRET_KEY")
 
     def _generate_access_token(self, payload: dict) -> str:
         payload['exp'] = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=24)
         return jwt.encode(payload, self.secret, algorithm='HS256')
 
-    def get_access_token(self, payload: dict) -> str:
-        return  self._generate_access_token(payload)
+    def _generate_refresh_token(self, payload: dict) -> str:
+        payload['exp'] = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=7)
+        return jwt.encode(payload, self.secret, algorithm='HS256')
 
+    def get_access_token(self, payload: dict) -> dict:
+        return {
+            "access_token": self._generate_access_token(payload),
+            "refresh_token": self._generate_refresh_token(payload)
+        }
 
     def decode_credentials(self, credentials):
         try:
