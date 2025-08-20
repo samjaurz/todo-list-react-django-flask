6fe4038d33c7876527d8cfdf141aff131ca62264
Samuel Jauregui <samjaurz_@hotmail.com>
2025-08-20 08:39:24 -0700

    refactor(decorator): switch retrieved token from cookies to authorized headers

--- flask-api/backend/flask/auth/decorator.py
+++ flask-api/backend/flask/auth/decorator.py
@@ -5,13 +5,16 @@ from functools import wraps
 def auth_decorator(func):
     @wraps(func)
     def wrapper(session, **kwargs):
-        token = request.cookies.get('access_token')
+        print(request.headers)
+        token = request.headers.get('Authorization').split()[1]
+        print(token)
         if not token:
             return jsonify({"error": "Token missing"}), 401
         decode = JWTAuth().decode_credentials(token)
         if not decode:
             return jsonify({"error": "This token is invalid"})
-
+        print(decode["user_id"])
+        request.user_id = decode["user_id"]
         return func(session,**kwargs)
     return wrapper
 
