8cc21a4f3b763c7b92ef9b60c3b5aba52039e256
Samuel Jauregui <samjaurz_@hotmail.com>
2025-08-20 14:38:59 -0700

    refacor(user_routes): modified endpoint to accept user_id injected

--- flask-api/backend/flask/routes/users_routes.py
+++ flask-api/backend/flask/routes/users_routes.py
@@ -20,34 +20,24 @@ def get_user_by_id(session, user_id: int):
 @auth_decorator
 def get_all_task_by_user(session, user_id: int):
     tasks = UserRepository(session).get_all_tasks_by_user(user_id)
-    if not tasks:
-        return jsonify({
-            "user_id": user_id,
-            "message": "tasks not found"}) , 404
     return jsonify({
         "user_id":user_id,
-        "tasks": tasks
+        "tasks": tasks or []
     }), 200
 
-@users_api.route('/search/', methods=['GET'])
+@users_api.route('/<int:user_id>/search/', methods=['GET'])
 @with_db_session
-def search_all_task_by_user(session):
-    token = request.cookies.get('access_token')
-    if not token:
-        return jsonify({"error": "Token missing"}), 401
-    decode = JWTAuth().decode_credentials(token)
-    if not decode:
-        return jsonify({"error": "This token is invalid"})
-
+@auth_decorator
+def search_all_task_by_user(session,user_id: int):
+    print(request.args.get('name'))
     name = request.args.get('name')
-
-    tasks = UserRepository(session).get_all_tasks_by_user(decode["user_id"])
+    tasks = UserRepository(session).get_all_tasks_by_user(user_id)
     filtered_tasks = [task for task in tasks if name.lower() in task['name'].lower()]
     if not tasks:
         return jsonify({
-            "user_id": decode["user_id"],
+            "user_id": user_id,
             "message": "tasks not found"}) , 401
     return jsonify({
-        "user_id":decode["user_id"],
+        "user_id":user_id,
         "tasks": filtered_tasks
     }), 200
